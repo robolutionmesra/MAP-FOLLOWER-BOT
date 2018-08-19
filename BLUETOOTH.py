b61efe66e91da8bb691b06866d5434bce61b3cb4
robolutionmesra <42320803+robolutionmesra@users.noreply.github.com>
2018-08-19 15:56:49 +0530

    Create BLUETOOTH.py

---
+++ BLUETOOTH.py
@@ -0,0 +1,43 @@
+'''
+author : SOURABH KHEMKA
+project done as intern at Roolution, BIT Mesra
+
+'''
+
+import serial
+
+port = "COM3"
+
+#bluetooth = serial.Serial(port,9600);
+print('CONNECTED');
+
+count = 0
+
+while(count<len(inst)):
+     
+    #SENDING MOTOR NUMBER
+   #print('###')
+   #bluetooth.read()
+   print('+++')
+   bluetooth.write(str(int(inst[count][0])))
+   #print(int(str(inst[count][0])))
+   #x = bluetooth.read()
+   #print x
+   if(bluetooth.read()=='2'):
+       print('ACKNOWLEDGED :)')
+   
+    #SENDING DISTANCE IN PIXELS
+    
+   bluetooth.write(str((int(inst[count][1]))/10))  
+   if(bluetooth.read()=='3'):
+       print('ACKNOWLEDGED :)')
+   
+    #SENDING ANGLE IN DEGREES [0,360)
+   
+   bluetooth.write(str(int(inst[count][2])))
+   if(bluetooth.read()=='1'):
+       print('ACKNOWLEDGED :)')
+    
+    #COUNTER INCREMENTATION
+   count += 1
+   bluetooth.read()
